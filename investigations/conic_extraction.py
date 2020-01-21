303b52c0f9d7ce98a0e372a908e6fce9f8e91234
Thomas Sundvoll <thomsun@stud.ntnu.no>
2020-01-21 21:15:33 +0100

    Worked on filling the ellipse

--- investigations/conic_extraction.py
+++ investigations/conic_extraction.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import random
-from math import pi
+import math
 from scipy import ndimage
 
 IMG_WIDTH = 640
@@ -281,15 +281,15 @@ def fit_ellipse(points):
     print(a)
     print
 
-    # # Drawing the ellipse
-    # delta = 0.025
-    # xrange = np.arange(0, IMG_WIDTH, delta)
-    # yrange = np.arange(0, IMG_HEIGHT, delta)
-    # X, Y = np.meshgrid(xrange,yrange)
+    # Drawing the ellipse
+    delta = 0.025
+    xrange = np.arange(0, IMG_WIDTH, delta)
+    yrange = np.arange(0, IMG_HEIGHT, delta)
+    X, Y = np.meshgrid(xrange,yrange)
 
-    # # F is one side of the equation, G is the other
-    # F = a[0]*X*X + a[1]*X*Y + a[2]*Y*Y + a[3]*X + a[4]*Y + a[5]
-    # G = 0
+    # F_ellipse is one side of the equation, G_ellipse is the other
+    F_ellipse = a[0]*X*X + a[1]*X*Y + a[2]*Y*Y + a[3]*X + a[4]*Y + a[5]
+    G_ellipse = 0
 
     mean_x = np.average(x)
     mean_y = np.average(y)
@@ -298,25 +298,86 @@ def fit_ellipse(points):
     M = np.array([[a[0][0], a[1][0]/2.0],
         [a[1][0]/2.0, a[2][0]]])
 
-    print(M)
+    A = a[0][0]
+    B = a[1][0]
+    C = a[2][0]
+    D = a[3][0]
+    E = a[4][0]
+    F = a[5][0]
+
+    print('\n')
+    print(A)
+    print(B)
+    print(C)
+    print(D)
+    print(E)
+    print(F)
+    print('\n')
+
+
+
+    x_0 = (2*C*D - B*E) / (B*B - 4*A*C) 
+    y_0 = (2*A*E - B*D) / (B*B - 4*A*C) 
+
+
+    print("x_0 and y_0:")
+    print(x_0)
+    print(y_0)
+    print('\n')
+
+
+    inner_square = math.sqrt( (A-C)**2 + B**2)
+
+    outside = 1 / (B**2 - 4*A*C)
+
+    a = outside * math.sqrt(2*(A*E**2 + C*D**2 - B*D*E + (B**2 - 4*A*C)*F) * ( (A+C) + inner_square))
+
+    b = outside * math.sqrt(2*(A*E**2 + C*D**2 - B*D*E + (B**2 - 4*A*C)*F) * ( (A+C) - inner_square))
+
+    print("a and b:")
+    print(a)
+    print(b)
+
+
+    if B != 0:
+        theta = math.atan( (1/B)*(C - A - math.sqrt( (A-C)**2 + B**2)))
+    else:
+        if A < C:
+            theta = 0
+        else:
+            theta = math.pi/2
+    
+    print("theta:")
+    print(theta)
+
+    
+
+
+    # print(M)
     eigenvalues, eigenvectors = np.linalg.eig(M)
     lambda_1 = eigenvalues[0]
     lambda_2 = eigenvalues[1]
-    print(eigenvalues)
+    # print(eigenvalues)
     S = np.linalg.det(M)
-    print(S)
+    # print(S)
 
-    x_center = -S/()
+    a = -S/(lambda_1*lambda_1*lambda_2)
+    b = -S/(lambda_1*lambda_2*lambda_2)
+
+    # print("a and b:")
+    # print(a)
+    # print(b)
     
 
     # plt.figure(figsize=(8.89,5))
-    background = plt.imread("image.jpg")
+    background = plt.imread("image_above.jpg")
     background = np.flipud(background)
-    plt.plot(mean_x, mean_y, 'go')
+    # plt.plot(mean_x, mean_y, 'go')
+    # plt.plot(x_0, y_0, 'bo')
     plt.imshow(background)
     plt.xlim((0,IMG_WIDTH))
     plt.ylim((0,IMG_HEIGHT))
-    # plt.contour(X, Y, (F - G), [0])
+    plt.contour(X, Y, (F_ellipse - G_ellipse), [0])
     plt.show()
 
 
@@ -326,7 +387,7 @@ def fit_ellipse(points):
     # a=2.     #radius on the x-axis
     # b=1.5    #radius on the y-axis
 
-    # t = np.linspace(0, 2*pi, 100)
+    # t = np.linspace(0, 2*math.pi, 100)
     # plt.figure(figsize=(8,8))
     # plt.plot( u+a*np.cos(t) , v+b*np.sin(t) )
     # plt.xlim((0,255))
@@ -362,6 +423,27 @@ def hsv_make_orange_to_green(hsv):
     imask = mask>0
     orange_to_green = hsv.copy()
     orange_to_green[imask] = hsv_green
+
+    return orange_to_green
+
+
+def hsv_keep_orange_only(hsv):
+    bgr_white = np.uint8([[[255,255,255]]])
+    hsv_white = cv2.cvtColor(bgr_white,cv2.COLOR_BGR2HSV)
+
+    bgr_black = np.uint8([[[0,0,0]]])
+    hsv_black = cv2.cvtColor(bgr_black,cv2.COLOR_BGR2HSV)
+
+    lower_orange = np.array([8,128,64])
+    upper_orange = np.array([28,255,255])
+    mask = cv2.inRange(hsv, lower_orange, upper_orange)
+
+    # change the orange to green
+    imask = mask>0
+    b_imask = mask<=0
+    orange_to_green = hsv.copy()
+    orange_to_green[imask] = hsv_white
+    orange_to_green[b_imask] = hsv_black
 
     return orange_to_green
 
@@ -407,7 +489,7 @@ def hsv_save_image(image, label='image', is_gray=False):
 
 
 def load_image():
-    filepath = 'image.jpg'
+    filepath = 'image_above.jpg'
 
     img = cv2.imread(filepath) # import as BGR
 
@@ -421,48 +503,162 @@ def load_image():
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     hsv = hsv_save_image(hsv, '1_hsv')
     # ### Preprocessing ###
-    hsv = hsv_save_image(hsv_make_orange_to_green(hsv), '2_orange to green')
+    hsv = hsv_save_image(hsv_make_orange_to_green(hsv), '2_orange_to_green')
     hsv = make_blurry(hsv, 9)
-    hsv = hsv_save_image(make_blurry(hsv, 3), '3_make blurry')
-    hsv = hsv_save_image(hsv_find_green_mask(hsv), '4_green mask')
+    hsv = hsv_save_image(make_blurry(hsv, 3), '3_make_blurry')
+    hsv = hsv_save_image(hsv_find_green_mask(hsv), '4_green_mask')
     hsv = make_blurry(hsv, 9)
-    hsv = hsv_save_image(make_blurry(hsv, 9), '5_make blurry_2')
-    gray = hsv_save_image(hsv_make_grayscale(hsv), '6_make grayscale', is_gray=True)
+    hsv = hsv_save_image(make_blurry(hsv, 9), '5_make_blurry_2')
+    gray = hsv_save_image(hsv_make_grayscale(hsv), '6_make_grayscale', is_gray=True)
     gray = make_blurry(gray, 31)
-    gray = hsv_save_image(make_blurry(gray, 21), '7_make blurry_3', is_gray=True)
+    gray = hsv_save_image(make_blurry(gray, 21), '7_make_blurry_3', is_gray=True)
 
     return gray
 
 
+def load_image_find_orange():
+    filepath = 'image_above.jpg'
 
-img = load_image()
-edges = cv2.Canny(img,100,200)
-print(type(edges))
+    img = cv2.imread(filepath) # import as BGR
+
+    # winname = "image"
+    # cv2.namedWindow(winname)        # Create a named window
+    # cv2.moveWindow(winname, 40,500)  # Move it to (40,30)
+    # cv2.imshow(winname, img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv = hsv_save_image(hsv, '1_hsv')
+    # ### Preprocessing ###
+    hsv = hsv_save_image(hsv_keep_orange_only(hsv), '2_orange_to_white')
+    # hsv = make_blurry(hsv, 9)
+    # hsv = hsv_save_image(make_blurry(hsv, 3), '3_make_blurry')
+    # hsv = hsv_save_image(hsv_find_green_mask(hsv), '4_green_mask')
+    # hsv = make_blurry(hsv, 9)
+    # hsv = hsv_save_image(make_blurry(hsv, 9), '5_make_blurry_2')
+    gray = hsv_save_image(hsv_make_grayscale(hsv), '6_make_grayscale', is_gray=True)
+    # gray = make_blurry(gray, 31)
+    # gray = hsv_save_image(make_blurry(gray, 21), '7_make_blurry_3', is_gray=True)
+
+    return gray
 
 
-unique, counts = np.unique(edges, return_counts=True)
-print(dict(zip(unique, counts)))
+def direction_from_point():
+    # filepath = 'image_above.jpg'
+    # img = cv2.imread(filepath) # import as BGR
 
-result = np.where(edges == 255)
-print(result)
+    point_x = 128
+    point_y = 247
 
-fit_ellipse(result)
-
-x = result[1]
-y = IMG_HEIGHT - result[0]
-
-# print(x)
-
-plt.figure(figsize=(8.89,5))
-plt.plot(x, y, 'go')
-plt.xlim((0,IMG_WIDTH))
-plt.ylim((0,IMG_HEIGHT))
-plt.show()
+    background = plt.imread("image_above.jpg")
+    background = np.flipud(background)
+    plt.plot(point_x, point_y, 'go')
+    plt.imshow(background)
+    plt.xlim((0,IMG_WIDTH))
+    plt.ylim((0,IMG_HEIGHT))
+    plt.show()
 
 
-winname = 'edges'
-cv2.namedWindow(winname)        # Create a named window
-cv2.moveWindow(winname, 40,500)  # Move it to (40,30)
-cv2.imshow(winname, edges)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+    K = np.array([[374.6706070969281, 0.0, 320.5],
+                  [0.0, 374.6706070969281, 180.5],
+                  [0.0, 0.0, 1.0]
+                ])
+
+    print(K)
+
+    # K: [374.6706070969281, 0.0, 320.5, 0.0, 374.6706070969281, 180.5, 0.0, 0.0, 1.0]
+
+
+def find_contours(img):
+    ret, thresh = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # create hull array for convex hull points
+    hull = []
+    
+    # calculate points for each contour
+    for i in range(len(contours)):
+        # creating convex hull object for each contour
+        hull.append(cv2.convexHull(contours[i], False))
+
+    # create an empty black image
+    drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
+    
+    # draw contours and hull points
+    for i in range(len(contours)):
+        color_contours = (0, 255, 0) # green - color for contours
+        color = (255, 0, 0) # blue - color for convex hull
+        # draw ith contour
+        cv2.drawContours(drawing, contours, i, color_contours, 1, 8, hierarchy)
+        # draw ith convex hull object
+        cv2.drawContours(drawing, hull, i, color, 1, 8)
+
+    gray = hsv_save_image(drawing, '0_hull', is_gray=True)
+
+
+def ellipse_detection():
+    img = load_image()
+
+    find_contours(img)
+    
+    return
+
+    edges = cv2.Canny(img,100,200)
+    print(type(edges))
+
+
+    unique, counts = np.unique(edges, return_counts=True)
+    print(dict(zip(unique, counts)))
+
+    result = np.where(edges == 255)
+    print()
+    print("Result:")
+    print(result)
+
+    fit_ellipse(result)
+
+    x = result[1]
+    y = IMG_HEIGHT - result[0]
+
+    # print(x)
+
+    background = plt.imread("image_above.jpg")
+    background = np.flipud(background)
+    plt.figure(figsize=(8.89,5))
+    plt.plot(x, y, 'ro')
+    plt.imshow(background)
+    plt.xlim((0,IMG_WIDTH))
+    plt.ylim((0,IMG_HEIGHT))
+    plt.show()
+
+
+    # winname = 'edges'
+    # cv2.namedWindow(winname)        # Create a named window
+    # cv2.moveWindow(winname, 40,500)  # Move it to (40,30)
+    # cv2.imshow(winname, edges)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+
+# ellipse_detection()
+
+
+def flood_fill():
+    im = load_image()
+    h,w = im.shape
+    seed = (w/2,h/2)
+
+    mask = np.zeros((h+2,w+2),np.uint8)
+
+    floodflags = 4
+    floodflags |= cv2.FLOODFILL_MASK_ONLY
+    floodflags |= (255 << 8)
+
+    num,im,mask,rect = cv2.floodFill(im, mask, seed, (255,0,0), (10,)*3, (10,)*3, floodflags)
+
+    cv2.imwrite("flood_fill.png", mask)
+
+    return mask
+
+flood_fill()
--- investigations/save_image.py
+++ investigations/save_image.py
@@ -3,9 +3,12 @@
 import rospy
 
 from sensor_msgs.msg import Image
+from nav_msgs.msg import Odometry
 import numpy as np
 from scipy.misc import imsave
 import os
+from scipy.spatial.transform import Rotation as R
+
 
 print(os.getcwd())
 os.chdir('/home/thomas/master_project/investigations')
@@ -13,6 +16,50 @@ print(os.getcwd())
 
 
 previous_image = None
+prev_rel_position = [None, None, None]
+
+PLATFORM_OFFSET_X = 2
+PLATFORM_OFFSET_Y = 2
+PLATFORM_OFFSET_Z = 0
+
+def position_callback(data):
+    global prev_rel_position
+
+    # prev_position[0] = data.pose.pose.position.x - PLATFORM_OFFSET_X
+    # prev_position[1] = data.pose.pose.position.y - PLATFORM_OFFSET_Y
+    # prev_position[2] = data.pose.pose.position.z - PLATFORM_OFFSET_Z
+
+    # global prev_ground_truth
+
+    # Transform ground truth in world frame to body frame
+
+    # Position
+    p_x = data.pose.pose.position.x
+    p_y = data.pose.pose.position.y
+    p_z = data.pose.pose.position.z
+
+    # Orientation
+    q_x = data.pose.pose.orientation.x
+    q_y = data.pose.pose.orientation.y
+    q_z = data.pose.pose.orientation.z
+    q_w = data.pose.pose.orientation.w
+
+    r = R.from_quat([q_x, q_y, q_z, q_w])
+    r_transpose = r.inv()
+    
+    offset_x = PLATFORM_OFFSET_X
+    offset_y = PLATFORM_OFFSET_Y
+    offset_z = PLATFORM_OFFSET_Z
+
+    p_0 = np.array([offset_x, offset_y, offset_z])      # The helipad's position in world coordinates
+    d_0_1 = np.array([p_x, p_y, p_z])      # Translation from origo in the world frame to origo in the body frame
+
+    diff = p_0 - d_0_1
+
+    p_1 = r_transpose.apply(diff)           # The helipad's position in body coordinates
+
+    prev_rel_position = p_1
+    # rospy.loginfo("Ground truth data " + str(prev_ground_truth))
 
 
 def video_callback(data):
@@ -45,6 +92,13 @@ def save_image():
     filepath = 'dataset/' + filename + filetype
     imsave(filepath, image_array)
 
+    f = open("position.txt", "w")
+    x_pos = str(prev_rel_position[0])
+    y_pos = str(prev_rel_position[1])
+    z_pos = str(prev_rel_position[2])
+    f.write('x_pos: ' + x_pos + ' | y_pos: ' + y_pos + ' | z_pos: ' + z_pos)
+    f.close()
+
     return True
 
 
@@ -52,7 +106,8 @@ def run():
     
     rospy.init_node('save_image', anonymous=True)
 
-    rospy.Subscriber('ardrone/front/image_raw', Image, video_callback)
+    rospy.Subscriber('ardrone/bottom/image_raw', Image, video_callback)
+    rospy.Subscriber('ground_truth/state', Odometry, position_callback)
 
     rospy.loginfo("Starting saving of images")
 
