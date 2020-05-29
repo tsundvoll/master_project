517e8c733d00a6de630c26a3909657c085b2bcb7
Thomas Sundvoll <thomsun@stud.ntnu.no>
2020-05-29 13:02:32 +0200

    Changed x-offset to 150 mm

--- catkin_ws/src/uav_vision/scripts/cv_module.py
+++ catkin_ws/src/uav_vision/scripts/cv_module.py
@@ -728,7 +728,7 @@ def calculate_position(center_px, radius_px):
     # Camera is placed 150 mm along x-axis of the drone
     # Since the camera is pointing down, the x and y axis of the drone
     # is the inverse of the x and y axis of the camera
-    est_x = -(est_z * d_x / focal_length) - 100 # mm adjustment for translated camera frame
+    est_x = -(est_z * d_x / focal_length) - 150 # mm adjustment for translated camera frame
     est_y = -(est_z * d_y / focal_length)
 
     est_z -= 45 # mm adjustment for translated camera frame
