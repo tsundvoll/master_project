da84921404b4a4ba8d75355522c971455c33a2ed
Thomas Sundvoll <thomsun@stud.ntnu.no>
2020-06-05 23:46:28 +0200

    Started on hover_error_compare plot

--- catkin_ws/src/uav_vision/scripts/plot_data.py
+++ catkin_ws/src/uav_vision/scripts/plot_data.py
@@ -162,7 +162,6 @@ def plot_data_manually(stored_array):
     # 37	est_error_arrow
     # 43	est_error_corners
     # 49	est_error_dead_reckoning
-
     index_values = [1, 7, 13, 19]
     color_values = ['green', 'blue', 'red', 'yellow']
     legend_values = ['ground truth', 'ellipse', 'arrow', 'corners']
@@ -193,12 +192,10 @@ def plot_data_manually(stored_array):
 
         line, = right_ax.plot(time_stamps_local, data)
         line.set_color(z_right_color)
-        # line.set_label(legend_text)
 
         right_ax.set_ylabel('z-position [m]', color='grey')
         right_ax.tick_params(axis='y', labelcolor='grey')
         right_ax.set_ylim(-0.07, 5.84)
-        # right_ax.legend(loc='upper right', facecolor='white', framealpha=1)
 
         if i == 1:
             variable = V_X
@@ -233,15 +230,10 @@ def plot_data_manually(stored_array):
             y_label = "yaw-rotation error [deg]"
             y_tics = np.linspace(0, 80, num=5)
 
-        else:
-            print "ERROR"
-            return 0
-
         # Label x-axis
         if i==7 or i==8:
             ax.set_xlabel('Time [s]')
 
-        
         # Plot the estimate values
         if i % 2 == 1:
             for j in range(4):
@@ -261,11 +253,8 @@ def plot_data_manually(stored_array):
                 line.set_color(color)
 
                 if i == 1:
-                    # line.set_label(legend_text if i==1 else "_nolegend_")
                     legend_lines.append(line)
             
-            # ax.set_ylabel(y_label)
-
         # Plot the estimate errors
         if i % 2 == 0:
             ax.axhline(y=0, color='grey', linestyle='--') # Plot the zero-line
@@ -321,21 +310,18 @@ def plot_hover_compare(hover_0_5m, hover_1m, hover_2m, hover_3m, hover_5m, hover
         np.linspace(-8, 8, num=9), np.linspace(0, 140, num=8), np.linspace(-8, 8, num=9)]
 
     for variable in [V_X, V_Y, V_Z, V_YAW]:
-    # for variable in [V_X]:
         file_title = file_titles[variable]
         y_label = y_labels[variable]
         fig = plt.figure(figsize=(10,12))
 
         for i in range(6):
             ax = plt.subplot(3,2,i+1)
-
             if i==4 or i==5:
                 ax.set_xlabel('Time [s]')
 
             title = titles[i]
             hover_data = all_hover_data[i]
             time_stamps = hover_data[:, 0]
-            # y_tics = all_ytics[variable]
 
             plt.title(title)
             plt.grid()
@@ -370,8 +356,80 @@ def plot_hover_compare(hover_0_5m, hover_1m, hover_2m, hover_3m, hover_5m, hover
 
             # Plot legend on top
             if i==1:
-                # ax.legend(bbox_to_anchor=(1.1, 1.1), loc='center', ncol=4)
-                # ax.legend(bbox_to_anchor=(0.7, 0.4, 0.8, 1.4), loc='center', ncol=4, mode='expand')
+                plt.legend(bbox_to_anchor=(-0.7, 1.2, 1.2, 0.0), loc='upper right', ncol=5, mode='expand')
+
+        fig.tight_layout()
+
+        folder = './plots/'
+        plt.savefig(folder+file_title+'.svg')
+
+
+def plot_hover_error_compare(hover_0_5m, hover_1m, hover_2m, hover_3m, hover_5m, hover_10m):
+    file_titles = ['Hover_error_x', 'Hover_error_y', 'Hover_error_z', 'None', 'None', 'Hover_error_yaw']
+    titles = ['z=0.5m','z=1m','z=1.8m','z=3m','z=5m','z=10m']
+    all_hover_data = np.array([hover_0_5m, hover_1m, hover_2m, hover_3m, hover_5m, hover_10m])
+    
+    index_values = [31, 37, 43]
+    color_values = ['blue', 'red', 'yellow']
+    legend_values = ['ellipse', 'arrow', 'corners']
+    y_labels = ['x-position error[m]', 'y-position error[m]', 'z-position error[m]', 'none', 'none', 'yaw-rotation error[m]']
+
+    x_ytics = [np.linspace(-0.10, 0.10, num=5)]*6
+    x_ytics[5] = np.linspace(-0.15, 0.10, num=6)
+
+    y_ytics = [np.linspace(-0.15, 0.15, num=7)]*6
+    z_ytics = [np.linspace(0.30, 0.70, num=5), np.linspace(0.80, 1.20, num=5), np.linspace(1.70, 2.10, num=5),
+        np.linspace(2.80, 3.20, num=5), np.linspace(4.80, 5.80, num=6), np.linspace(9.80, 10.20, num=5)]
+    yaw_ytics = [np.linspace(-8, 8, num=9), np.linspace(-8, 8, num=9), np.linspace(-8, 8, num=9),
+        np.linspace(-8, 8, num=9), np.linspace(0, 140, num=8), np.linspace(-8, 8, num=9)]
+
+    for variable in [V_X, V_Y, V_Z, V_YAW]:
+        file_title = file_titles[variable]
+        y_label = y_labels[variable]
+        fig = plt.figure(figsize=(10,12))
+
+        for i in range(6):
+            ax = plt.subplot(3,2,i+1)
+            if i==4 or i==5:
+                ax.set_xlabel('Time [s]')
+
+            title = titles[i]
+            hover_data = all_hover_data[i]
+            time_stamps = hover_data[:, 0]
+
+            plt.title(title)
+            plt.grid()
+            plt.xlim(time_stamps[0], time_stamps[-1])
+
+            for j in range(3):
+                if variable==V_YAW and j==0: # Skip the ellipse yaw estimate
+                    continue
+                index = index_values[j]
+                color = color_values[j]
+                legend_text = legend_values[j]
+            
+                data = hover_data[:, index:index+6][:,variable]
+            
+                time_stamps_local = time_stamps.copy()
+                time_stamps_local[np.isnan(data)] = np.nan
+        
+                line, = ax.plot(time_stamps_local, data)
+                line.set_color(color)
+                line.set_label(legend_text if i==1 else "_nolegend_")
+            
+            ax.set_ylabel(y_label)
+
+            if variable == V_X:
+                ax.set_yticks(x_ytics[i])
+            # elif variable == V_Y:
+            #     ax.set_yticks(y_ytics[i])
+            # elif variable == V_Z:
+            #     ax.set_yticks(z_ytics[i]) 
+            # elif variable == V_YAW:
+            #     ax.set_yticks(yaw_ytics[i])
+
+            # Plot legend on top
+            if i==1:
                 plt.legend(bbox_to_anchor=(-0.7, 1.2, 1.2, 0.0), loc='upper right', ncol=5, mode='expand')
 
         fig.tight_layout()
@@ -381,13 +439,6 @@ def plot_hover_compare(hover_0_5m, hover_1m, hover_2m, hover_3m, hover_5m, hover
 
 
 if __name__ == '__main__':
-
-    # if len(sys.argv) == 2:
-    #     test_number = sys.argv[1]
-    # else:
-    #     test_number = 1
-    # print "Plotting test number " + str(test_number)
-
     # Load the data
     folder = './catkin_ws/src/uav_vision/data_storage/'
     
@@ -431,59 +482,11 @@ if __name__ == '__main__':
 
 
 
-
-    # Plot the data
-    plot_data_manually(up_and_down_5m)
+    #################
+    # Plot the data #
+    #################
+    # plot_data_manually(up_and_down_5m)
 
     # plot_hover_compare(hover_0_5m, hover_1m, hover_2m, hover_3m, hover_5m, hover_10m)
-
-
-    # # Settings
-    # plot_error = False
-    # plot_z_to_the_right = True
-
-    # # 0: x, 1: y, 2: z, 3: roll, 4: pitch, 5: yaw
-    # # variables_to_plot = [0, 1, 2, 5]
-    # variables_to_plot = [0, 1, 2, 5]
-
-
-    # 0: ground truth, 1: ellipse, 2: arrow, 3: corners, 4: dead reckoning
-    # 5: ellipse_error, 6: arrow_error, 7: corners_error, 8: dead reckoning_error
-    # if plot_error:
-    #     methods_to_plot = [5, 6, 7]
-    #     plot_z_to_the_right = True
-    #     z_right_color = 'grey'
-    # else:
-    #     methods_to_plot = [0, 1, 2, 3]
-    #     plot_z_to_the_right = True
-    #     z_right_color = 'grey'
     
-    # Data
-    # 0: time
-
-    # 1: ground truth
-    
-    # 2: est_ellipse
-    # 3: est_arrow
-    # 4: est_corners
-    # 5: est_dead_reckoning
-
-    # 6: est_error_ellipse
-    # 7: est_error_arrow
-    # 8: est_error_corners
-    # 9: est_error_dead_reckoning
-    
-    
-    # plot_error = False
-    # methods_to_plot = [0, 1, 2, 3]
-    # plot_z_to_the_right = True
-    # z_right_color = 'grey'
-    # plot_data(stored_array, methods_to_plot, variables_to_plot, plot_error, plot_z_to_the_right, z_right_color)
-    
-    # plot_error = True
-    # methods_to_plot = [5, 6, 7]
-    # plot_z_to_the_right = True
-    # z_right_color = 'grey'
-    # plot_data(stored_array, methods_to_plot, variables_to_plot, plot_error, plot_z_to_the_right, z_right_color)
-
-
+    plot_hover_error_compare(hover_0_5m, hover_1m, hover_2m, hover_3m, hover_5m, hover_10m)
